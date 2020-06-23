b57292457461e40ecb956e2070bab395c978d64f
czerebro <kapakuly@yandex.ru>
2020-06-23 17:58:25 +0300

    use base operations with numbers

---
+++ lesson5.py
@@ -0,0 +1,34 @@
+#add
+print(2 + 3)
+
+#sub
+print(2 - 3)
+print(-5 - 3)
+
+#umnogenie
+print(2 * 3)
+
+#div -> float
+print(6 / 3)
+
+#modul
+print(6 % 2) #6 - 2 - 2 - 2 = 0
+print(7 % 2) #7 - 2 - 2 -2 = 1
+
+#div null
+print(12 // 5) #12 / 5 = 2 (2,4)
+print(7 // 2) #7 / 2 = 3 (3,5)
+
+#stepen
+print(5 ** 2)
+print(9 ** 0.5) #kvadratn. koren -> float
+
+#multi operations
+print(2 + 3 * 4) #14
+print((2 + 3) * 4) #20
+
+#multy type operation
+print(2 + 0.5) #2.5 -> float
+
+#to4nost operations
+print(0.1 + 0.2) #0.30000000000000004
