71d800362d0b60f860155b509e33beae6df75e97
czerebro <kapakuly@yandex.ru>
2020-06-24 10:57:51 +0300

    added multi set var

--- lesson6.py
+++ lesson6.py
@@ -3,6 +3,7 @@ my_var1 = 1
 _myvar1 = 3
 myVar = 7.0
 var = None
+var1, var2 = (2, 9)
 #1my_var -> error
 
 #opredelenie type var
@@ -10,7 +11,7 @@ print(type(my_var1))  #int
 print(type(myVar))    #float
 
 #use multi print
-print(my_var1, _myvar1, myVar, var)
+print(my_var1, _myvar1, myVar, var, var1, var2)
 
 #use const
 TEST = 5
