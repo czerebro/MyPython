6a14887457ab83a9bf6e91fbd6ac13f0f318b1eb
czerebro <kapakuly@yandex.ru>
2020-06-24 10:54:31 +0300

    added using var = None

--- lesson6.py
+++ lesson6.py
@@ -2,6 +2,7 @@
 my_var1 = 1
 _myvar1 = 3
 myVar = 7.0
+var = None
 #1my_var -> error
 
 #opredelenie type var
@@ -9,7 +10,7 @@ print(type(my_var1))  #int
 print(type(myVar))    #float
 
 #use multi print
-print(my_var1, _myvar1, myVar)
+print(my_var1, _myvar1, myVar, var)
 
 #use const
 TEST = 5
