50d5594b26c6fa34714a06c7498c606205110913
czerebro <kapakuly@yandex.ru>
2020-06-24 09:46:17 +0300

    uses var in python

---
+++ lesson6.py
@@ -0,0 +1,15 @@
+#use peremennie
+my_var1 = 1
+_myvar1 = 3
+myVar = 7.0
+#1my_var -> error
+
+#opredelenie type var
+print(type(my_var1))  #int
+print(type(myVar))    #float
+
+#use multi print
+print(my_var1, _myvar1, myVar)
+
+#use const
+TEST = 5
