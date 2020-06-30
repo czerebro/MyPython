9b87c5b93be51f5b91ac420c31fe13527efd00bb
czerebro <kapakuly@yandex.ru>
2020-06-30 16:29:15 +0300

    add other codes of task

--- task18.py
+++ task18.py
@@ -9,6 +9,10 @@ for i in range(0, len(l2)):
     l4.append(l2[i] * 2)
 print(l4)
 
+# l1 = [1, 2, 3]
+# l2 = [i * 2 for i in l1]
+# print(l2)
+
 """
 2. Дан список. Возведите в квадрат каждый из его элементов и получит сумму всех полученных квадратов:
 [1, 2, 3] --> 14 --> 1^2 + 2^2 + 3^2 = 14
@@ -19,6 +23,12 @@ k = 0
 for i in range(0, len(l)):
     k = k + l[i] * l[i]
 print(k)
+
+# l1 = [1, 2, 3]
+# res = 0
+# for num in l1:
+#     res += num ** 2
+# print(res)
 
 """
 3. Игорь любит кататься на велосипеде. Он знает, что при этом важно не допустить обезвоживания и пьет 0,5 литра воды в час. 
@@ -31,6 +41,18 @@ time3 = 11.8 --> litres = 5
 time = float(input("Введите время: "))
 litr = time * 0.5
 print(f"За {time}ч выпито {int(litr)}л")
+
+# time1 = 3 # litres = 1
+# time2 = 6.7 # litres = 3
+# time3 = 11.8 # litres = 5
+#
+# print(int(time1 // 2))
+# print(int(time2 // 2))
+# print(int(time3 // 2))
+#
+# print(int(time1 / 2))
+# print(int(time2 / 2))
+# print(int(time3 / 2))
 
 """
 4. Дана строка 'Hello world'. Проверьте, если в этой строке есть символ пробела - " ", тогда преобразуйте строку к верхнему регистру, если же нет, тогда к нижнему.
@@ -46,3 +68,12 @@ s = 'Hello world'
 for l in s:
     if l == ' ':
         print(s.upper())
+    else:
+        print(s.lower())
+
+#s = 'Hello world'
+#if ' ' in s:
+#    s = s.upper()
+#else:
+#    s = s.lower()
+#print(s)
