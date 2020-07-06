e9bbdad309b91ed9ba1dbf4ea3535a3ea6e1c20a
czerebro <kapakuly@yandex.ru>
2020-07-06 17:37:26 +0300

    добавил решение от автора задачи

--- task20.py
+++ task20.py
@@ -8,13 +8,20 @@
 words = ['мадам', 'топот', 'test', 'madam', 'word']
 my_str = ['Око за око', 'А роза упала на лапу Азора', 'Около Миши молоко']
 
-'''for i in words:
+for i in words:
     newList = list((i))
     newList2 = list(newList)
     newList2.reverse()
     if newList != newList2:
         words.remove(i)
-print(words)'''
+print(words)
+
+#palindromes = []
+# for word in words:
+#     if word == word[::-1]:
+#         palindromes.append(word)
+# palindromes = [word for word in words if word == word[::-1]]
+# print(palindromes)
 
 #ver1.1
 
@@ -25,6 +32,15 @@ for i in my_str:
     if new_str != new_str2:
         my_str.remove(i)
 print(my_str)
+
+# my_str = ['Око за око', 'А роза упала на лапу Азора', 'Около Миши молоко']
+# palindromes = []
+# for word in my_str:
+#     word_r = word.replace(' ', '').lower()
+#     if word_r == word_r[::-1]:
+#         palindromes.append(word)
+#
+# print(palindromes)
 
 
 
