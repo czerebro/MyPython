f731abb42125b03f33753485720ac79a4a221e86
czerebro <kapakuly@yandex.ru>
2020-07-06 16:59:52 +0300

    вычленил из списка предложения полиндромы произвольным решением

--- task20.py
+++ task20.py
@@ -4,16 +4,29 @@
 Дан список my_str со строками, часть из которых являются палиндромами. Составьте новый список строк-палиндромов.
 """
 
+#ver1
 words = ['мадам', 'топот', 'test', 'madam', 'word']
 my_str = ['Око за око', 'А роза упала на лапу Азора', 'Около Миши молоко']
 
-for i in words:
+'''for i in words:
     newList = list((i))
     newList2 = list(newList)
     newList2.reverse()
     if newList != newList2:
         words.remove(i)
-print(words)
+print(words)'''
+
+#ver1.1
+
+for i in my_str:
+    new_str = [j for j in i.lower() if j != ' ']
+    new_str2 = list(new_str)
+    new_str2.reverse()
+    if new_str != new_str2:
+        my_str.remove(i)
+print(my_str)
+
+
 
 
 
