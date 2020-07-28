66b4ccbab8e81772b34bf9bab322ad34720fe032
czerebro <kapakuly@yandex.ru>
2020-07-28 16:24:20 +0300

    добавил решение 2й задачи и решения автора задач

--- task28.py
+++ task28.py
@@ -27,6 +27,11 @@ print(y)
 def odd_ball(arr):
     return bool([item for item in arr if item == arr.index('odd')])
 
+"""
+def odd_ball(arr):
+    return arr.index('odd') in arr
+"""
+
 print(odd_ball(["even",4,"even",7,"even",55,"even",6,"even",10,"odd",3,"even"])) # True
 print(odd_ball(["even",4,"even",7,"even",55,"even",6,"even",9,"odd",3,"even"])) # False
 print(odd_ball(["even",10,"odd",2,"even"])) # True
@@ -39,11 +44,30 @@ print(odd_ball(["even",10,"odd",2,"even"])) # True
 """
 
 def find_sum(n):
-    return n
+    res = 0
+    for item in range(n + 1):
+        if item%3 == 0 or item%5 == 0:
+            res += item
+    return res
+
 
 print(find_sum(5)) # return 8 (3 + 5)
 print(find_sum(10)) # return 33 (3 + 5 + 6 + 9 + 10)
 
+
+"""
+def find_sum(n):
+    res = 0
+    for i in range(n + 1):
+        if i % 3 == 0 or i % 5 == 0:
+            res += i
+    return res
+
+
+def find_sum2(n):
+    return sum(i for i in range(n + 1) if i % 3 == 0 or i % 5 == 0)
+
+"""
 
 """
 3. Дан список имен. Выберите в новый список только те имена, которые состоят из 4-х букв.
@@ -53,6 +77,11 @@ names = ["Ryan", "Kieran", "Mark", "John", "David", "Paul"] # ["Ryan", "Mark", "
 def get_names(names):
     [names.remove(name) for name in names if len(name) != 4]
     return names
+
+"""
+def get_names(names):
+    return [i for i in names if len(i) == 4]
+"""
 
 
 names = ["Ryan", "Kieran", "Mark", "John", "David", "Paul"]
